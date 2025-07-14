e6e732565f81a5f9e9cc28da893ceaf2d4952635
timothee lux <luxtimothee@gmail.com>
2025-07-14 23:22:24 +0100

    Added an initial start to my notes-app to learn react native

---
+++ app/index.jsx
@@ -0,0 +1,65 @@
+import PostItNote from '@/assets/images/postItNote.png';
+import { useRouter } from 'expo-router';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const HomeScreen = () => {
+  const router = useRouter();
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={PostItNote} style={styles.image}></Image>
+      <Text style={styles.title}>Welcome to Notes App</Text>
+      <Text style={styles.subtitle}>Capture your thoughts</Text>
+      <TouchableOpacity
+        style = {styles.button}
+        onPress={() => router.push('/notes')}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: '#f8f9fa'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#666',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+});
+
+export default HomeScreen;
